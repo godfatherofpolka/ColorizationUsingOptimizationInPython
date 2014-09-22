fbaa953665207eb95becf8a1aa0377dda4592e00
godfatherofpolka <bucheli@datacomm.ch>
2014-09-22 16:38:38 +0100

    some comments

--- colorizer.py
+++ colorizer.py
@@ -8,7 +8,7 @@ This code is based on
   http://www.cs.huji.ac.il/~yweiss/Colorization/
 
 Example usage:
-  $ python colorizer.py example.png example_marked.png output.png --view
+  $ python colorizer.py inputGrey.png inputMarked.png output.png --view
 
 """
 import argparse
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 from colorizationSolver import colorize
                  
 def main():
+  # parse command line arguments
   parser = argparse.ArgumentParser(description='Colorize pictures')
   parser.add_argument('greyImage', help='png image to be coloured')
   parser.add_argument('markedImage', help='png image with colour hints')
@@ -25,14 +26,20 @@ def main():
   parser.add_argument('-v', '--view', help='display image', action='store_true')
   args = parser.parse_args()
 
-  # Note: when reading .png, the division by 255. is not required
+  # Note: when reading .png, division by 255. is not required
+  # Note: when reading .bmp, division by 255. is required
+  # TODO: make this more universal, i.e., support various image formats
+  # read images
   greyImage = mpimg.imread(args.greyImage, format='png')
   markedImage = mpimg.imread(args.markedImage, format='png')
   
+  # colorize
   colouredImage = colorize(greyImage, markedImage)
     
+  # save output
   mpimg.imsave(args.output,colouredImage, format='png')
   
+  # display output, if requested
   if args.view:
     plt.imshow(colouredImage)
     plt.show()
